14093714cc48f2ac8045b5b594f074640f3e234a
Igor Gudymenko <igorgudymenko@gmail.com>
2015-02-22 18:18:11 +0200

    implementation of warning message

---
+++ src/components/warningMessage/warning.service.js
@@ -0,0 +1,19 @@
+angular.module('warningService', []).factory('warningService', function() {
+  function checkChanges() {
+
+  }
+
+  function showMessage() {
+
+  }
+
+  function redirectToState() {
+
+  }
+
+  return {
+    checkChanges: checkChanges,
+    showMessage: showMessage,
+    redirectToState: redirectToState
+  }
+});
